945ab0eb2c13b0bfa335902dfd2742d916350c5a
johnkevin <johnkevinvegaolivo@gmail.com>
2023-04-29 22:41:23 -0400

    primer commit de proyecto almacen

---
+++ catalogo/carrito.js
@@ -0,0 +1,78 @@
+class Carrito {
+
+    //añadir producto al carrito 
+    comprarProducto(e){
+        e.preventDefault();
+        if(e.target.classList.contains('agregar-carrito')){
+            const producto = e.target.parentElement.parentElement;
+            this.leerDatosProducto(producto);
+        }
+    }
+
+    leerDatosProducto(producto){
+        const infoProducto = {
+            imagen : producto.querySelector('img').src,
+            name: producto.querySelector('h5').textContent,
+            precio : producto.querySelector('h6').textContent,
+            id : producto.querySelector('a').getAttribute('data-id'),
+            cantidad: 1
+        }
+        this.insetarCarrito(infoProducto);
+    }
+
+    insetarCarrito(producto){
+        const fila = document.createElement('tr');
+        fila.innerHTML = `
+            <td>
+                <img src="${producto.imagen}" width=100>
+            </td>
+            <td>${producto.name}</td>
+            <td>${producto.precio}</td>
+            <td>
+                <a href="#" class="borrar-producto" data-id="${producto.id}"><img src="borrar.png"></a>
+            </td>
+
+        `;
+        listaProductos.appendChild(fila);
+        this.guardarProductoLocalStorage(producto);
+    }
+
+    eliminarProducto(e){
+        e.preventDefault();
+        let producto, productoID;
+        if (e.target.classList.contains('borrar-producto')){
+            e.target.parentElement.parentElement.remove();
+            producto = e.target.parentElement.parentElement;
+            productoID= producto.querySelector('a').getAttribute('data-id');
+        }
+    }
+
+    vaciarCarrito(e){
+        e.preventDefault();
+        while (listaProductos.firstChild) {
+            listaProductos.removeChild(listaProductos.firstChild);
+        }
+        return false;
+    }
+
+    guardarProductoLocalStorage(producto){
+        let productos;
+        productos = this.obtenerProductosLocalStorage();
+        productos.push(producto);
+        localStorage.setItem('productos', JSON.stringify(productos));
+    }
+
+    obtenerProductosLocalStorage(){
+        let productoLS;
+
+        if(localStorage.getItem('productos') === null){
+            productoLS = [];
+        } else {
+            productoLS = JSON.parse(localStorage.getItem('productos'));
+        }
+        return productoLS;
+    }
+}
+
+
+
---
+++ catalogo/main.js
@@ -0,0 +1,133 @@
+let dataProductos = {
+  data: [
+    {id: '001', name: "platano", type: "verdura", price: "300", img: "platano.png" },
+    {id: '002', name: "manzana", type: "verdura", price: "200", img: "manzana.png" },
+    {id: '003', name: "lechuga", type: "verdura", price: "500", img: "lechuga.png" },
+    {id: '004', name: "zanahoria", type: "verdura", price: "100", img: "zanahoria.png" },
+    {id: '005', name: "morron", type: "verdura", price: "170", img: "morron.png" },
+    {id: '006', name: "brocoli", type: "verdura", price: "400", img: "brocoli.png" },
+    {id: '007', name: "coliflor", type: "verdura", price: "350", img: "coliflor.png" },
+    {id: '008', name: "kiwi", type: "verdura", price: "120", img: "kiwi.png" },
+    {id: '009', name: "esponja", type: "aseo", price: "300", img: "esponja.png" },
+    {id: '010', name: "ariel", type: "aseo", price: "2000", img: "ariel.png" },
+    {id: '011', name: "cif", type: "aseo", price: "1500", img: "cif.png" },
+    {id: '012', name: "escoba", type: "aseo", price: "1750", img: "escoba.png" },
+    {id: '013', name: "guantes", type: "aseo", price: "150", img: "guantes.png" },
+    {id: '014', name: "cocacola", type: "gaseosa", price: "2000", img: "coca.png" },
+    {id: '015', name: "fanta", type: "gaseosa", price: "1600", img: "fanta.png" },
+    {id: '016', name: "sprite", type: "gaseosa", price: "1350", img: "sprite.png" },
+    {id: '017', name: "monster", type: "gaseosa", price: "1500", img: "monster.png" },
+    {id: '018', name: "gatorade", type: "gaseosa", price: "1200", img: "gatorade.png" },
+    {id: '019', name: "yogurt", type: "lacteos", price: "200", img: "yogurt.png" },
+    {id: '020', name: "queso", type: "lacteos", price: "4000", img: "queso.png" },
+    {id: '021', name: "huevos", type: "lacteos", price: "300", img: "huevos.png" },
+    {id: '022', name: "leche", type: "lacteos", price: "950", img: "leche.png" },
+    {
+      id: '023',
+      name: "quesoblanco",
+      type: "lacteos",
+      price: "3000",
+      img: "quesoBlanco.png",
+    },
+    { id: '024',name: "espagueti", type: "cocina", price: "600", img: "espagueti.png" },
+    { id: '025',name: "arroz", type: "cocina", price: "700", img: "arros.png" },
+    { id: '026',name: "azucar", type: "cocina", price: "800", img: "azucar.png" },
+    { id: '027',name: "sal", type: "cocina", price: "800", img: "sal.png" },
+    { id: '028',name: "aceite", type: "cocina", price: "1100", img: "aceite.png" },
+    { id: '029',name: "gomitas", type: "dulces", price: "20", img: "gomitas.png" },
+    { id: '030',name: "malvaviscos", type: "dulces", price: "50", img: "malvaviscos.png" },
+    { id: '031',name: "chocolate", type: "dulces", price: "200", img: "chocolate.png" },
+    { id: '032',name: "piruletas", type: "dulces", price: "150", img: "piruletas.png" },
+    { id: '033',name: "oreo", type: "dulces", price: "500", img: "oreo.png" },
+  ],
+};
+
+
+
+
+//con este for iremos recorriendo la lista y creando las tarjetas de los productos
+for (let i of dataProductos.data) {
+  let tarjeta = document.createElement("div");
+  tarjeta.classList.add("tarjeta", i.type, "hide");//hide: esconder
+  //img div  
+  let imgContainer = document.createElement("div");
+  imgContainer.classList.add("imagen-contenedor");
+  //img
+  let imagen = document.createElement("img");
+  imagen.setAttribute("src", i.img);
+  imgContainer.appendChild(imagen);
+  tarjeta.appendChild(imgContainer);
+  //contenedor  
+  let contenedor = document.createElement("div");
+  contenedor.classList.add("contenedor");
+  //nombre productos
+  let name = document.createElement("h5");
+  name.classList.add("producto-name");
+  name.innerText = i.name.toUpperCase();
+  contenedor.appendChild(name);
+  //precio
+  let price = document.createElement("h6");
+  price.innerText = "$" + i.price;
+  contenedor.appendChild(price);
+  //button comprar
+  let compra = document.createElement("a");
+  compra.classList.add('agregar-carrito')
+  compra.setAttribute("data-id", i.id);
+  compra.href = '#';
+  compra.innerText = "Comprar";
+  contenedor.appendChild(compra);
+
+
+
+  tarjeta.appendChild(contenedor);
+  document.getElementById("catalogoProductos").appendChild(tarjeta);
+}
+
+function filtroProducto(valor) {
+  //ingresamos a la clase button
+  let boton = document.querySelectorAll(".button-valor");
+  boton.forEach((button) => {
+    //con eso comprobamos si el valor es igual y le daremos un estilo al estar activo
+    if (valor.toUpperCase() == button.innerText.toUpperCase()) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  //vamos a seleccionar todas las tarjetas
+  let elementos = document.querySelectorAll(".tarjeta");
+  elementos.forEach((elemento) => {
+    //con esto mostraremos todas las tarjetas al presionar el boton de todos
+    if (valor == "todos") {
+      elemento.classList.remove("hide");
+    } else {
+      //verificaremos si el elemento tiene la categoria, al no ser asi el elemento se ocultara
+      if (elemento.classList.contains(valor)) {
+        elemento.classList.remove("hide");
+      } else {
+        elemento.classList.add("hide");
+      }
+    }
+  });
+}
+
+document.getElementById("buscador").addEventListener('click',() => {
+  let buscadorInput = document.getElementById('buscador-input').value;
+  let elemen = document.querySelectorAll('.producto-name');
+  let tarjet = document.querySelectorAll('.tarjeta')
+
+  //recorremos todos los elementos y comprovamos si el texto tiene el valor de busqueda
+  elemen.forEach((elemento, index) => {
+    if (elemento.innerText.includes(buscadorInput.toUpperCase())){
+      tarjet[index].classList.remove("hide");
+    } else {
+      tarjet[index].classList.add("hide");
+    }
+  });
+});
+
+
+window.onload = () =>{
+  filtroProducto('todos');
+};
